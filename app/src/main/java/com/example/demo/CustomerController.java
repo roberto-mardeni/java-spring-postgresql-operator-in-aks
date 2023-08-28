9b4db141f884d26c7dd6f4b1978a4363dcc3ef3b
roberto-mardeni <rmardeni@gmail.com>
2023-08-28 19:10:18 -0400

    Completed changes

---
+++ app/src/main/java/com/example/demo/CustomerController.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Customer;
+import com.example.demo.CustomerRepository;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@Resource
+	CustomerRepository customerRepository;
+
+	@GetMapping(value = "/")
+	public List<Customer> getAll() {
+		List<Customer> results = new ArrayList<Customer>();
+		customerRepository.findAll().forEach(results::add);
+		return results;
+	}
+
+	@PostMapping(value = "/")
+	public void create(@RequestBody Customer c) {
+		customerRepository.save(c);
+	}
+
+	@PutMapping(value = "/")
+	public void update(@RequestBody Customer c) {
+		customerRepository.save(c);
+	}
+
+	@DeleteMapping(value = "/")
+	public void delete(@RequestBody Customer c) {
+		customerRepository.delete(c);
+	}
+}
---
+++ app/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.customers (
+        id,
+        first_name,
+        last_name,
+        address,
+        email
+    )
+VALUES (1, 'Jack', 'Black', 'USA', 'jack@gmail.com'),
+    (2, 'Peter', 'Thomas', 'USA', 'peter@gmail.com'),
+    (3, 'Joe', 'Smith', 'CAN', 'joe@gmail.com'),
+    (4, 'Thomas', 'McCormick', 'USA', 'thomas@gmail.com'),
+    (5, 'Carlos', 'Fernandes', 'MEX', 'carlos@gmail.com');
