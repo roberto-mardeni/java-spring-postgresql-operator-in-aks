2ef5a3324dc1a7457dfb66fc27ddcf12eff956bf
roberto-mardeni <rmardeni@gmail.com>
2023-08-29 11:47:48 -0400

    cleanup

--- app/src/main/java/com/example/demo/CustomerRepository.java
+++ app/src/main/java/com/example/demo/CustomerRepository.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
