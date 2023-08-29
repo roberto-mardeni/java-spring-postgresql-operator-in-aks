0bd310363cfa6a4ceec155138e6ef103e13ed93e
roberto-mardeni <rmardeni@gmail.com>
2023-08-29 16:24:54 -0400

    Disabled stage

---
+++ deploy/setup.cmd
@@ -0,0 +1,3 @@
+az aks show --resource-group java-postgresql --name myaks --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName -o table
+
+68fda521a69443e6a606.eastus.aksapp.io
