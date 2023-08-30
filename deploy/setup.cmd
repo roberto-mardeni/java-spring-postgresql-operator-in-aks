a260d804089ef795484a737c1ea46a441f7a5ef0
roberto-mardeni <rmardeni@gmail.com>
2023-08-29 20:53:45 -0400

    Removed unnecessary file

--- deploy/setup.cmd
+++
@@ -1,3 +0,0 @@
-az aks show --resource-group java-postgresql --name myaks --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName -o table
-
-68fda521a69443e6a606.eastus.aksapp.io
